f444fb7dcc320651595eacd82a2a56da69744464
Alfredo Miranda <alfredo@miranda.io>
2021-04-01 19:54:07 +0100

    Implement log viewer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
-import "./App.css";
-import { Button, CssBaseline } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import { CssBaseline, Typography } from "@material-ui/core";
+import { FixedSizeList as List } from "react-window";
+
+import fetchLogs from "./fetchLogs";
+
+const Row = ({ index, style, data }) => {
+  return <div style={style}>{data.logs[index]}</div>;
+};
+
+function computeStats(logs, stats) {
+  let { info, warning, error } = stats;
+
+  logs.forEach((log) => {
+    if (log.includes("INFO")) {
+      info++;
+    } else if (log.includes("WARNING")) {
+      warning++;
+    } else if (log.includes("ERROR")) {
+      error++;
+    }
+  });
+
+  return { info, warning, error };
+}
 
 function App() {
+  const [logs, setLogs] = useState([]);
+  const [stats, setStats] = useState({ info: 0, warning: 0, error: 0 });
+
+  useEffect(() => {
+    let isCurrent = true;
+    const interval = setInterval(() => {
+      fetchLogs().then((data) => {
+        if (isCurrent) {
+          const newLogs = data.split("\n");
+          setLogs((logs) => [...logs, ...newLogs]);
+          setStats((stats) => computeStats(newLogs, stats));
+        }
+      });
+    }, 1000);
+
+    return () => {
+      isCurrent = false;
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <>
       <CssBaseline />
-
-      <Button color="primary">Hello World</Button>
+      <Typography>Log viewer</Typography>
+      <div>Statistics</div>
+      <div>INFO: {stats.info}</div>
+      <div>WARNINGS: {stats.warning}</div>
+      <div>ERRORS: {stats.error}</div>
+      <List
+        height={500}
+        itemCount={logs.length}
+        itemSize={35}
+        width={500}
+        itemData={{ logs }}
+      >
+        {Row}
+      </List>
     </>
   );
 }
---
+++ src/fetchLogs.js
@@ -0,0 +1,24 @@
+const messageTypes = [
+  "INFO Some info message",
+  "WARNING Some warning message",
+  "ERROR Some error message",
+];
+
+function fetchLogs() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const numLogs = Math.floor(10 * Math.random()) + 1;
+      const logs = new Array(numLogs);
+      const t = Date.now();
+
+      for (let i = 0; i < numLogs; i++) {
+        const messageType = Math.floor(3 * Math.random());
+        const dt = new Date(t + i * 1000).toISOString();
+        logs[i] = `${dt.slice(0, dt.length - 1)} ${messageTypes[messageType]}`;
+      }
+      resolve(logs.join("\n"));
+    }, 800);
+  });
+}
+
+export default fetchLogs;

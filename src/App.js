06ceeb525b74c8c48fb5c33e1fddb2db288cebd9
Alfredo Miranda <alfredo@miranda.io>
2021-04-02 17:00:56 +0100

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import { CssBaseline, Typography } from "@material-ui/core";
+import {
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  Typography,
+  Grid,
+  makeStyles,
+} from "@material-ui/core";
 import LogsViewer from "./features/logsViewer/LogsViewer";
 
+const useStyles = makeStyles({
+  appbar: {
+    marginBottom: "2rem",
+  },
+});
+
 function App() {
+  const classes = useStyles();
   return (
     <>
       <CssBaseline />
-      <Typography>Log viewer</Typography>
-      <LogsViewer />
+      <AppBar className={classes.appbar} position="sticky">
+        <Toolbar>
+          <Typography variant="subtitle1">Logs Viewer</Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container justify="center">
+        <LogsViewer />
+      </Grid>
     </>
   );
 }
--- src/features/logsViewer/LogsViewer.js
+++ src/features/logsViewer/LogsViewer.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { FixedSizeList as List } from "react-window";
 import { useSelector, useDispatch } from "react-redux";
+import { Grid, Paper, Typography, makeStyles } from "@material-ui/core";
 import { selectStats, updateStats } from "./statsSlice";
 import { selectLogs, updateLogs } from "./logsSlice";
 import fetchLogs from "../../fetchLogs";
@@ -9,7 +10,18 @@ function Row({ index, style, data }) {
   return <div style={style}>{data.logs[index]}</div>;
 }
 
+const useStyle = makeStyles({
+  statsContainer: {
+    padding: "2rem",
+    marginBottom: "1rem",
+  },
+  logsContainer: {
+    padding: "2rem",
+  },
+});
+
 function LogsViewer() {
+  const classes = useStyle();
   const logs = useSelector(selectLogs);
   const stats = useSelector(selectStats);
   const dispatch = useDispatch();
@@ -33,21 +45,25 @@ function LogsViewer() {
   }, [dispatch]);
 
   return (
-    <>
-      <div>Statistics</div>
-      <div>INFO: {stats.info}</div>
-      <div>WARNINGS: {stats.warning}</div>
-      <div>ERRORS: {stats.error}</div>
-      <List
-        height={500}
-        itemCount={logs.length}
-        itemSize={35}
-        width={500}
-        itemData={{ logs }}
-      >
-        {Row}
-      </List>
-    </>
+    <Grid item>
+      <Paper className={classes.statsContainer}>
+        <Typography variant="h6">Statistics</Typography>
+        <div>INFO: {stats.info}</div>
+        <div>WARNINGS: {stats.warning}</div>
+        <div>ERRORS: {stats.error}</div>
+      </Paper>
+      <Paper className={classes.logsContainer}>
+        <List
+          height={600}
+          itemCount={logs.length}
+          itemSize={35}
+          width={550}
+          itemData={{ logs }}
+        >
+          {Row}
+        </List>
+      </Paper>
+    </Grid>
   );
 }
 
